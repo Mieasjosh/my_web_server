6bbdbeafbc9bb621e82236ef3033c049cc4afe19
Mieav <3498487729@qq.com>
2026-06-05 00:03:30 +0800

    可以允许了，但是还差一个界面上传文件

--- threadpool/threadpool.h
+++ threadpool/threadpool.h
@@ -38,7 +38,7 @@ threadpool<T>::threadpool(int actor_model,connection_pool *connPool,int thread_n
 {
     if(thread_number<=0 || max_requests<=0)
         throw exception();
-    m_threads=new pthread_t(m_thread_number);
+    m_threads=new pthread_t[m_thread_number];
     if(!m_threads) throw exception();
     for(int i=0;i<m_thread_number;i++)
     {
